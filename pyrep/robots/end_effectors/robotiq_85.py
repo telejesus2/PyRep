51c814048c6543a7bcee63e22fac0f4b95b5432a
telejesus2 <jebumagoma@gmail.com>
2021-01-25 10:34:22 +0100

    remove my robotiq85 files

--- pyrep/robots/end_effectors/robotiq_85.py
+++
@@ -1,8 +0,0 @@
-from pyrep.robots.end_effectors.gripper import Gripper
-
-
-class Robotiq85(Gripper):
-
-    def __init__(self, count: int = 0):
-        super().__init__(count, 'ROBOTIQ_85',
-                         ['ROBOTIQ_85_RactiveJoint', 'ROBOTIQ_85_LactiveJoint'])
